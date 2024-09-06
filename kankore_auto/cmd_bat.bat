88db8a1188a639311fb31a1098f7e26315b5c80a
Minopon0626 <bahxn934@gmail.com>
2024-09-06 12:42:51 +0900

    バグっているので削除

--- kankore_auto/cmd_bat.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM コマンドプロンプトを開く
-start cmd /k "cd /d %~dp0 && virtual_environment\Scripts\activate.bat"
--- kankore_auto/stop_action.py
+++
@@ -1,5 +0,0 @@
-def stop_action(app):
-    # タイマーを停止する処理（ここではシンプルな例として表示のみ）
-    app.update_display(current_task="タイマー停止中", next_time="タイマー停止")
-    app.start_button.config(state="normal")  # スタートボタンを再度有効にする
-    app.stop_button.config(state="disabled")  # ストップボタンを無効にする
