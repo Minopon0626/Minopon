a2fc4bc15e35805923a8312ab7f4f582eb04dbe7
Minopon0626 <bahxn934@gmail.com>
2024-09-07 23:05:25 +0900

    マウス移動の非同期処理を行うように

--- kankore_auto/bauxite_action.py
+++ kankore_auto/bauxite_action.py
@@ -1,9 +1,11 @@
-from utility_functions import delayed_click
+from utility_functions import delayed_click, delayed_update
 
 def bauxite_click(root, current_task_label):
     # ラベルを「ボーキ処理中...」に更新
-    current_task_label.config(text="ボーキ処理中...")
+    #current_task_label.config(text="ボーキ処理中...")
+    delayed_update(root, current_task_label, 0, "ボーキ処理中...")
     # 3秒遅延でマウス移動し、さらに2秒後にクリック
-    delayed_click(root, 100, 100)
+    delayed_click(root, 300, 360)
     
-    current_task_label.config(text="ボーキ完了")
+    # 5秒後に「ボーキ完了」に更新する
+    delayed_update(root, current_task_label, 5000, "ボーキ完了")
--- kankore_auto/utility_functions.py
+++ kankore_auto/utility_functions.py
@@ -10,9 +10,31 @@ def delayed_click(root, x, y):
     :param y: クリックするY座標
     """
     # 3秒後にマウスを移動
-    root.after(3000, lambda: pyautogui.moveTo(x, y))
-    print(f"マウスを移動予定 - 座標 ({x}, {y})")
+    root.after(3000, lambda: [
+        pyautogui.moveTo(x, y),  # マウスを移動させて
+        print(f"マウスを移動しました - 座標 ({x}, {y})"),
 
-    # さらに2秒後にクリック
-    root.after(2000, lambda: pyautogui.click(x, y))
-    print(f"クリック予定 - 座標 ({x}, {y})")
+        # さらに2秒後にクリック
+        root.after(2000, lambda: [
+            pyautogui.click(x, y),
+            print(f"クリック完了 - 座標 ({x}, {y})")
+        ])
+    ])
+
+
+def delayed_update(root, label, delay_ms, new_text):
+    """
+    Tkinterのafterメソッドを使用して、指定された時間（ミリ秒）の遅延後に
+    ラベルの表示内容を変更する関数。
+
+    :param root: Tkinterのrootウィンドウ（afterメソッドを利用するため）
+    :param label: 表示内容を変更するTkinterのラベル
+    :param delay_ms: 遅延時間（ミリ秒）
+    :param new_text: 遅延後に表示するテキスト
+
+    使用例:
+    delayed_update(root, current_task_label, 5000, "ボーキ完了")
+
+    また遅延秒数を0とすると即座に反映される
+    """
+    root.after(delay_ms, lambda: label.config(text=new_text))
